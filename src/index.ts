328c0b83f30475c1164294db0c833dab858d7145
Liu Yue <hangxingliu@gmail.com>
2024-08-16 02:29:39 +0800

    update exported items

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-// basic utilities
-export * from "./utils/download";
-export * from "./utils/env";
-export { getHumanReadableFileSize } from "./utils/file";
-export { networkRetry } from "./utils/network-retry";
+//#region azure blob
+export * from "./utils/azure/blob/blob-sas.js";
+export * from "./utils/azure/blob/block-helper.js";
+export * from "./utils/azure/blob/copy-blob.js";
+export * from "./utils/azure/blob/del-blob.js";
+export * from "./utils/azure/blob/list-blob.js";
+export * from "./utils/azure/blob/put-blob.js";
+export * from "./utils/azure/blob/put-block-list.js";
+export * from "./utils/azure/blob/put-block.js";
+//#endregion azure blob
 
-// azure utilities
-export * from "./utils/azure/blob/blob-sas";
-export * from "./utils/azure/blob/block-helper";
-export * from "./utils/azure/blob/copy-blob";
-export * from "./utils/azure/blob/del-blob";
-export * from "./utils/azure/blob/list-blob";
-export * from "./utils/azure/blob/put-blob";
-export * from "./utils/azure/blob/put-block-list";
-export * from "./utils/azure/blob/put-block";
-export * from "./utils/azure/crypto";
-export * from "./utils/azure/env";
-export * from "./utils/azure/result-parser";
-export * from "./utils/azure/shared-key-lite";
-export { getContentTypeByExt } from "./utils/azure/content-type";
-export {
-  AzureConnectInfo,
-  getAzureBlobHost,
-  getAzureProtocol,
-  getAzureBlobURL,
-} from "./utils/azure/types";
-export { decodeXml } from "./utils/azure/xml-entities";
+//#region azure algo/utils
+export * from "./utils/azure/content-type.js";
+export * from "./utils/azure/crypto.js";
+export * from "./utils/azure/env.js";
+export * from "./utils/azure/result-parser.js";
+export * from "./utils/azure/shared-key-lite.js";
+export * from "./utils/azure/types.js";
+export { decodeXml, decodeXmlEntity } from "./utils/azure/xml-entities.js";
+//#endregion azure algo/utils
+
+//#region basic utils
+export * from "./utils/download.js";
+export { loadEnvFiles, readEnvFile } from "./utils/env.js";
+export { getHumanReadableFileSize } from "./utils/file.js";
+export { Logger } from "./utils/logger.js";
+export { networkRetry } from "./utils/network-retry.js";
+//#endregion basic utils
